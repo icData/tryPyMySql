1621109970289ec69221e699499b9bb5cb0f9c26
Maoji Wang <maoji.wang@cims.nyu.edu>
2017-11-01 16:50:23 +0800

    add two ways to get a col from a table

--- pymysql_connect.py
+++ pymysql_connect.py
@@ -11,7 +11,7 @@
 #
 #      COPYRIGHT 2017
 #
-#      Description:
+#      All rights reserved
 #
 #  ==============================================
 
@@ -28,23 +28,53 @@ config = {
           'cursorclass':pymysql.cursors.DictCursor,
           }
 
-# Connect to the database
+# Connect to a database
 connection = pymysql.connect(**config)
 
-# Show all tables
-cursor=connection.cursor()
+# Show all tables of the database
+cursor = connection.cursor()
 cursor.execute("show tables;")
 result = cursor.fetchall()
 connection.commit()
 for i in result: print(i.values())
 
+# Show all col-attributes of a table
+cursor.execute("desc t_cif_bank;")
+result = cursor.fetchall()
+connection.commit()
+print(result[0].keys())
+for i in result: print(i.values())
+
 # Show all lines in a table
-cursor = connection.cursor()
 cursor.execute("select * from t_cif_bank;")
 result = cursor.fetchall()
 connection.commit()
 print(result[0].keys())
 for i in result: print(i.values())
+
+# Get a col from a table
+cursor.execute("select * from t_cif_bank;")
+result = cursor.fetchall()
+connection.commit()
+print(result[0].keys())
+# for i in result: print(list(i.values())[4])
+x = [list(i.values())[4] for i in result]
+#        ^--- got the dict value of each result line;
+#             then convert it to list, and
+#             pick the 4-th elem.  Lastly,
+#             rearrange them back to another list.
+print('the 4-th col is:', x)
+
+# Get a certain col from a table --- a better way:
+this_attr = 'bankCode'
+cursor.execute("select %s from t_cif_bank;"%this_attr)
+result = cursor.fetchall()
+connection.commit()
+print(result[0].keys())
+# for i in result: print(list(i.values())[0])
+x = [list(i.values())[0] for i in result]
+#  same with the above group, only that, it is always 0 nowhere
+print('the %s col is:'%this_attr, x)
 
 cursor.close()
 connection.close()
